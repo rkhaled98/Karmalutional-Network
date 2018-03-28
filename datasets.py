256d04c863e1b179c450686e8a0be70dd7bc4441
Rafi Khaled <rkhaled@vassar.edu>
2018-03-27 20:54:24 -0400

    we are using Python... not JavaScript

--- datasets.py
+++ datasets.py
@@ -5,7 +5,7 @@ import praw
 def clean_csv(file, print_sub_id = False, output_file = False):
     df = pd.read_csv(file)
     df[['7']].sub(df[['8']])  # subtract ups from downs
-    df = df.iloc[:, [1,2,7]] if print_sub_id else df.iloc[:, [1,7]] # get only comment and score columns unless specified to get id
+    df = df.iloc[1:4, [1,2,7]] if print_sub_id else df.iloc[:, [1,7]] # get only comment and score columns unless specified to get id
     df.rename(columns={'0': 'comment', '1': 'id', '6': 'score'}, inplace=True)
 
     df = df.dropna(axis=0, how='any')  # delete any row with no data in either column
@@ -29,13 +29,10 @@ def login(password): # login as username karmalutionalNetwork
                          user_agent='karmalutionalNetwork', username='karmalutionalNetwork')
 
 def convert_comment_id_to_submission_id(comment_id, reddit): # attempt to get thread id
-    try{
+    try:
         submission_id = reddit.comment(id=comment_id).submission
         return submission_id
-    }
-    catch(err){
+    except Exception as e:
         return "fail"
-    }
-
 
 clean_csv(file='news_news.csv', print_sub_id = True, output_file = True)
