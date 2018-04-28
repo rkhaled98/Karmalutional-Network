1e2f636c8868c1fabb0a63dbbacc0d64ecb86818
Colby Morrison <colbyacts@gmail.com>
2018-04-28 12:42:27 -0400

    pythonic things

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='karmalutional-network',
+    version='1.0.0.dev1',
+    description='Using Recurrent Neural Networks to predict Reddit comment scores'
+)
