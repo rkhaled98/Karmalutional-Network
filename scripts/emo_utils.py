a9ffbed27f91b96150a4edfcb3ef6656676f0c0e
Rafi Khaled <rkhaled@vassar.edu>
2018-05-10 21:29:17 -0400

    begin script changes

--- scripts/emo_utils.py
+++ scripts/emo_utils.py
@@ -51,17 +51,15 @@ def convert_to_one_hot(Y, C):
     return Y
 
 
-emoji_dictionary = {"0": "\u2764\uFE0F",    # :heart: prints a black instead of red heart depending on the font
-                    "1": ":baseball:",
-                    "2": ":smile:",
-                    "3": ":disappointed:",
-                    "4": ":fork_and_knife:"}
+emoj_dictionary = {"0": "heart",    # :heart: prints a black instead of red heart depending on the font
+                    "1": "baseball",
+                    "2": "smile",
+                    "3": "disappointed",
+                    "4": "fork_and_knife"}
 
 def label_to_emoji(label):
-    """
-    Converts a label (int or string) into the corresponding emoji code (string) ready to be printed
-    """
-    return emoji.emojize(emoji_dictionary[str(label)], use_aliases=True)
+    """Converts a label (int or string) into the corresponding emoji code (string) ready to be printed"""
+    return emoj_dictionary[str(label)]
               
     
 def print_predictions(X, pred):
